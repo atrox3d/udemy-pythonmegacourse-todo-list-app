0be1f1eb931aa2dd153863da66a433d9851fd7de
atrox3d <atrox3d@gmail.com>
2022-11-29 07:29:58 +0100

    setup project

---
+++ main.py
@@ -0,0 +1,17 @@
+while True:
+    user_action = input("Type add, show, edit, complete or exit: ").strip()
+
+    print(f'user action: {user_action}')
+    match user_action:
+        case 'add':
+            pass
+        case 'show':
+            pass
+        case 'edit':
+            pass
+        case 'complete':
+            pass
+        case 'exit':
+            break
+
+print('goodbye')
