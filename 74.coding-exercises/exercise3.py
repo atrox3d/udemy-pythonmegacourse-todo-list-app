2309e03622d81b21db6150860090f8ed8ee3e4c4
atrox3d <atrox3d@gmail.com>
2022-11-30 09:54:53 +0100

    74.coding exercises

---
+++ 74.coding-exercises/exercise3.py
@@ -0,0 +1,3 @@
+user_entries = ['10', '19.1', '20']
+
+print([float(entry) for entry in user_entries])
---
+++ 74.coding-exercises/exercise4.py
@@ -0,0 +1,3 @@
+user_entries = ['10', '19.1', '20']
+
+print(sum([float(entry) for entry in user_entries]))
