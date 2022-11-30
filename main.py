2ee7ebb6ad52e787e58729c067230f4aad2fc0c9
atrox3d <atrox3d@gmail.com>
2022-11-30 21:06:27 +0100

    use with context manager

--- main.py
+++ main.py
@@ -7,20 +7,17 @@ while True:
         case 'add':
             todo = input("Enter a todo:") + "\n"
 
-            file = open("todos.txt", 'r')           # FileNotFoundError if file does not exist
-            todos = file.readlines()
-            file.close()
+            with open("todos.txt", 'r') as file:    # FileNotFoundError if file does not exist
+                todos = file.readlines()
 
             todos.append(todo)
 
-            file = open("todos.txt", 'w')
-            file.writelines(todos)
-            file.close()
+            with open("todos.txt", 'w') as file:
+                file.writelines(todos)
 
         case 'show':
-            file = open("todos.txt", 'r')           # FileNotFoundError if file does not exist
-            todos = file.readlines()
-            file.close()
+            with open("todos.txt", 'r') as file:    # FileNotFoundError if file does not exist
+                todos = file.readlines()
 
             for index, item in enumerate(todos):
                 item = item.strip('\n')
