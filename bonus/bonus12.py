0dab38f2e9719979905d7c57906dcbbc3acd32e4
atrox3d <atrox3d@gmail.com>
2022-12-03 07:07:04 +0100

    132. bonus12 - decoupling

--- bonus/bonus12.py
+++ bonus/bonus12.py
@@ -1,13 +1,19 @@
-def convert(feet_inches):
+def parse_feet_inches(feet_inches):
     parts = feet_inches.split(" ")
     feet, inches = [float(n) for n in parts]
+    return feet, inches
 
+
+def convert(feet, inches):
     meters = feet * 0.3048 + inches * 0.0254
     return meters
 
 
-result = convert("3 12")
-if result < 1:
+feet, inches = parse_feet_inches("3 12")
+meters = convert(feet, inches)
+
+print(f"{feet} feet and {inches} inches is equal to {meters} meters")
+if meters < 1:
     print("kid too small")
 else:
     print("kid can use the slide")
