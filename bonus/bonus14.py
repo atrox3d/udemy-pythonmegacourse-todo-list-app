0c4e3dfecf29dcf4e4244d98cd1f92544995c3a0
atrox3d <atrox3d@gmail.com>
2022-12-03 07:50:25 +0100

    bonus14

---
+++ bonus/bonus14.py
@@ -0,0 +1,13 @@
+from bonus.converters14 import convert
+from bonus.parsers14 import parse_feet_inches
+
+feet, inches = parse_feet_inches("3 12")
+meters = convert(feet, inches)
+
+print(f"{feet} feet and {inches} inches is equal to {meters} meters")
+if meters < 1:
+    print("kid too small")
+else:
+    print("kid can use the slide")
+
+
