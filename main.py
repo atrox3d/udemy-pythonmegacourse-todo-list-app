9d8065bcc263a8db1ddb62d2231303a4cd367431
atrox3d <atrox3d@gmail.com>
2022-12-03 06:20:42 +0100

    130. Documenting the Code #doc-strings

--- main.py
+++ main.py
@@ -1,10 +1,16 @@
 def get_todos(filename="todos.txt"):
+    """
+    return a list of to-do items read from a file
+    """
     with open(filename, 'r') as file:  # FileNotFoundError if file does not exist
         todos = file.readlines()
         return todos
 
 
 def write_todos(todos, filename="todos.txt"):
+    """
+    write to-do items list to file
+    """
     with open(filename, 'w') as file:
         file.writelines(todos)
 
