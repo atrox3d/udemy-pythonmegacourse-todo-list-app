4004590fca549dd4d02e9831cd852fdd90a98a33
atrox3d <atrox3d@gmail.com>
2022-12-03 11:17:28 +0100

    refactor bonus6 files

--- bonus/bonus6.py
+++ bonus/bonus6.py
@@ -7,6 +7,6 @@ contents = [
 filenames = ["doc.txt", "report.txt", "presentation.txt"]
 
 for content, filename in zip(contents, filenames):
-    file = open(f'../files/{filename}', 'w')
+    file = open(f'files/{filename}', 'w')
     file.write(content)
     file.close()
