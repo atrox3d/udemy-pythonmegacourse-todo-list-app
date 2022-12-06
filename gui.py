c974cfa24408075e416f1a6376ef797fdb7b7877
atrox3d <atrox3d@gmail.com>
2022-12-06 09:58:55 +0100

    178. Implement "Complete" and "Exit" Buttons #gui-buttons

--- gui.py
+++ gui.py
@@ -4,6 +4,10 @@ import PySimpleGUI as sg
 label = sg.Text("Enter a TODO:")
 inputbox = sg.InputText(tooltip="enter a TODO", key="todo")
 addbutton = sg.Button("Add")
+completebutton = sg.Button("Complete")
+editbutton = sg.Button("Edit")
+exitbutton = sg.Button("Exit")
+
 
 listbox = sg.Listbox(
     values=functions.get_todos(),
@@ -11,22 +15,24 @@ listbox = sg.Listbox(
     enable_events=True,
     size=(45, 10)
 )
-editbutton = sg.Button("Edit")
 
 window = sg.Window(
     title='My ToDo app',
-    layout=[                    # layout rows
-        [label],                # row #1
-        [inputbox, addbutton],  # row #2
-        [listbox, editbutton]
+    layout=[                                    # layout rows
+        [label],                                # row #1
+        [inputbox, addbutton],                  # row #2
+        [listbox, editbutton, completebutton],  # row #4
+        [exitbutton]                            # row #5
     ],
     font=('Helvetica', 20)
 )
 
 while True:
     event, values = window.read()
-    print(f"INFO | event : {event}")
-    print(f"INFO | values: {values}")
+    print(f"INFO | event          : {event}")
+    print(f"INFO | values         : {values}")
+    if values:
+        print(f"INFO | values['todos']: {values['todos']}")
     match event:
         case sg.WIN_CLOSED:
             print("INFO | WIN_CLOSED event detected, closing...")
@@ -56,6 +62,20 @@ while True:
             functions.write_todos(todos)
 
             window['todos'].update(values=todos)
+
+        case 'Complete':
+            todos = functions.get_todos()
+
+            todo_to_complete = values['todos'][0]
+            todos.remove(todo_to_complete)
+
+            functions.write_todos(todos)
+
+            window['todos'].update(values=todos)
+            window['todo'].update(value='')
+
+        case "Exit":
+            break
 
         case 'todos':
             value = values['todos'][0]
