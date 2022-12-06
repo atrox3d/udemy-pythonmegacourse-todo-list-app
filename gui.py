eea00c1f74d305d0ebe752afb944974123176d9f
atrox3d <atrox3d@gmail.com>
2022-12-06 11:02:28 +0100

    179. Final Touches

--- gui.py
+++ gui.py
@@ -1,6 +1,11 @@
 import functions
 import PySimpleGUI as sg
+import time
 
+
+sg.theme("Black")
+
+clock = sg.Text(time.strftime("%b %d, %Y %H:%M:%S"), key="clock")
 label = sg.Text("Enter a TODO:")
 inputbox = sg.InputText(tooltip="enter a TODO", key="todo")
 addbutton = sg.Button("Add")
@@ -19,21 +24,30 @@ listbox = sg.Listbox(
 window = sg.Window(
     title='My ToDo app',
     layout=[                                    # layout rows
-        [label],                                # row #1
-        [inputbox, addbutton],                  # row #2
-        [listbox, editbutton, completebutton],  # row #4
-        [exitbutton]                            # row #5
+        [clock],                                # row 1
+        [label],                                # row 2
+        [inputbox, addbutton],                  # row 3
+        [listbox, editbutton, completebutton],  # row 5
+        [exitbutton]                            # row 6
     ],
     font=('Helvetica', 20)
 )
 
 while True:
-    event, values = window.read()
-    print(f"INFO | event          : {event}")
-    print(f"INFO | values         : {values}")
-    if values:
-        print(f"INFO | values['todos']: {values['todos']}")
+    event, values = window.read(timeout=1000, timeout_key="clock")
+    window["clock"].update(value=time.strftime("%b %d, %Y %H:%M:%S"))
+
+    if event != "clock":
+        print(f"INFO | event          : {event}")
+        print(f"INFO | values         : {values}")
+        try:
+            print(f"INFO | values['todos']: {values['todos']}")
+        except TypeError:
+            pass
+
     match event:
+        case "clock":
+            pass
         case sg.WIN_CLOSED:
             print("INFO | WIN_CLOSED event detected, closing...")
             break
@@ -49,36 +63,41 @@ while True:
             window['todos'].update(values=todos)
 
         case 'Edit':
-            todos = functions.get_todos()
+            try:
+                todos = functions.get_todos()
 
-            if not len(values['todos']):
-                print(f"INFO | ignoring Edit event because values['todos'] is empty")
-                continue
-            todo = values['todos'][0]
-            new_todo = values['todo'] + "\n"
-            index = todos.index(todo)
-            todos[index] = new_todo
+                todo = values['todos'][0]
+                new_todo = values['todo'] + "\n"
+                index = todos.index(todo)
+                todos[index] = new_todo
 
-            functions.write_todos(todos)
+                functions.write_todos(todos)
 
-            window['todos'].update(values=todos)
+                window['todos'].update(values=todos)
+            except IndexError:
+                sg.popup("Please select an item first", font=("helvetica", 20))
 
         case 'Complete':
-            todos = functions.get_todos()
+            try:
+                todos = functions.get_todos()
 
-            todo_to_complete = values['todos'][0]
-            todos.remove(todo_to_complete)
+                todo_to_complete = values['todos'][0]
+                todos.remove(todo_to_complete)
 
-            functions.write_todos(todos)
+                functions.write_todos(todos)
 
-            window['todos'].update(values=todos)
-            window['todo'].update(value='')
-
+                window['todos'].update(values=todos)
+                window['todo'].update(value='')
+            except IndexError:
+                sg.popup("Please select an item first", font=("helvetica", 20))
         case "Exit":
             break
 
         case 'todos':
             value = values['todos'][0]
             window['todo'].update(value=value)
+
+        case _:
+            print("default")
 
 window.close()
