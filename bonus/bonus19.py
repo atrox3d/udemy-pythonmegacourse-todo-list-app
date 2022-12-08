ef3c7c7f9c71a9cd591921a267948cbcd54b9559
atrox3d <atrox3d@gmail.com>
2022-12-08 16:59:29 +0100

    193. bonus19.py

---
+++ bonus/bonus19.py
@@ -0,0 +1,34 @@
+import streamlit as st
+from PIL import Image
+import pathlib
+
+with st.expander("Start Camera"):
+    camera_image = st.camera_input("Camera")
+
+print("camera_image", camera_image)
+st.write("camera_image", camera_image)
+
+if camera_image:
+    image_path = pathlib.Path("camera-photos", camera_image.name)
+    with open(image_path, 'wb') as image_file:
+        image_file.write(camera_image.getbuffer())
+        print(f"file {image_path} saved")
+        st.write(f"file {image_path} saved")
+
+    img = Image.open(camera_image)
+
+    gray_img = img.convert("L")
+
+    st.image(gray_img)
+
+    image_path = pathlib.Path("camera-photos", camera_image.name)
+    grey_image_path = pathlib.Path(
+        image_path.parent,
+        image_path.stem + "GRAY" + image_path.suffix
+    )
+    gray_img.save(grey_image_path)
+    print(grey_image_path, "saved")
+    st.write(grey_image_path, "saved")
+
+
+
